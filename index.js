b24b861316aab73012a3210f6a6386b46ed8c110
briankipchirchir <kbrianngetich@gmail.com>
2024-07-21 21:19:10 +0300

    k

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// helpers.js
+
+function changeTextOnLoad() {
+    document.addEventListener('DOMContentLoaded', function() {
+      const paragraph = document.querySelector('p');
+      if (paragraph) {
+        paragraph.textContent = "This is really cool!";
+      }
+    });
+  }
+  changeTextOnLoad();
